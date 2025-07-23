b38b962fbb7cd21bb17d904b9d3d31b23615ca7c
Gustavo-Stedile <gustavostedile1@hotmail.com>
2025-07-23 10:40:45 -0300

    testando queries básicas

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
+from pyspark.sql.functions import col
 from pyspark.sql import SparkSession
+
+from pyspark.sql.types import (
+    StructType, StructField, StringType, IntegerType, BooleanType
+)
 
 spark = SparkSession.builder \
     .appName("TestApp") \
     .getOrCreate()
 
-df = spark.read \
-    .option("multiLine", "true") \
-    .json('people.json')
 
-df.show()
+def Mensagem():
+    return StructType([
+        StructField("mensagem", StringType()),
+        StructField("nome", StringType()),
+        StructField("idade", IntegerType()),
+        StructField("cidade", StringType()),
+        StructField("data_envio", StringType()),
+        StructField("hora_envio", StringType()),
+        StructField("dispositivo", StringType()),
+        StructField("tipo_mensagem", StringType()),
+        StructField("comprimento_mensagem", StringType()),
+        StructField("grupo", BooleanType()),
+    ])
+
+
+df = spark.read \
+    .schema(Mensagem()) \
+    .option("multiLine", "true") \
+    .json('mensagens.json')
+
+df.filter(col("comprimento_mensagem") > 2).show()
